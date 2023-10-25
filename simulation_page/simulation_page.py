887e05605d667b4f2415b3477fa637ef2e26b438
tzexinl <e0772781@u.nus.edu>
2023-10-25 13:51:15 +0800

    Added buttons for user to obtain bigger graph.

--- simulation_page/simulation_page.py
+++ simulation_page/simulation_page.py
@@ -1,23 +1,13 @@
 import dash
+import os
 import plotly.express as px 
-import plotly.graph_objs as go
 import pandas as pd
-from dash import html, dcc, Input, Output 
-from datetime import datetime, timedelta
-import random
+import dash_bootstrap_components as dbc
+from dash import html, dcc, Input, Output, State
 
 # Initializing the app 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Sample data for four library floors
-floors = ["Floor 3", "Floor 4", "Floor 5", "Floor 6"]
-time_range = [datetime(2023, 1, 1, 9, 0), datetime(2023, 1, 1, 18, 0)]
-time_interval = timedelta(minutes=15)
-timestamps = [time_range[0] + i * time_interval for i in range(int((time_range[1] - time_range[0]).total_seconds() / time_interval.total_seconds()))]
-
-data = {
-    floor: [random.randint(0, 100) for _ in range(len(timestamps))] for floor in floors
-}
 
 # Dummy inputs 
 df = pd.DataFrame({ # dummy results 
@@ -25,14 +15,6 @@ df = pd.DataFrame({ # dummy results
     'Change in Occupancy': [-5, 10, 50, -40, 10]
 })
 
-# The overall skeleton 
-app.layout = html.Div([
-    dcc.Tabs(id = 'tabs', value = 'BarPlot',children = [
-        dcc.Tab(label = 'Overall Change in Occupancy', value = 'tab-1'),
-        dcc.Tab(label = 'Occupancy overtime', value = 'tab-2')
-    ]),
-    html.Div(id = 'tab-content')
-])
 
 # Bar plots for 'Overall Change in Occupancy'
 fig_lvl3 = px.bar(df, x = 'Furniture Set', y = 'Change in Occupancy', title = 'Level 3', color = 'Change in Occupancy', 
@@ -58,60 +40,124 @@ fig_lvl6.update_traces(marker=dict(color=['red' if value <0 else 'green' for val
 # Layout for the 'Overall Change in Occupancy' tab
 tab_bp_layout = html.Div([
     html.Div([
-        dcc.Graph(figure = fig_lvl3), 
-    ], style = {'width':'48%', 'height' :'48%', 'display':'inline-block'}),
+        dcc.Graph(id = 'Graph_1', figure = fig_lvl3),
+        dbc.Button("Full Graph", id = "button_lvl3", n_clicks = 0, style = {'float':'right', 'background-color':'black'}),
+        html.Div(id = "popup-content", children = [
+            dbc.Modal([
+                dbc.ModalHeader("Level 3", style = {'background-color':'grey'}),
+                dbc.ModalBody([
+                    dcc.Graph(id = 'Graph_1', figure = fig_lvl3),
+                    ],
+                    style = {'background-color':'white'}),
+            ], id = "popup", fullscreen = True, centered = True)
+        ]),
+    ], style = {'width':'50%', 'height' :'50%', 'display':'inline-block'}),
+
     html.Div([
-        dcc.Graph(figure = fig_lvl4)
-    ], style = {'width':'48%', 'display': 'inline-block'}),
+        dcc.Graph(id = 'Graph_2', figure = fig_lvl4),
+        dbc.Button("Full Graph", id = "button_lvl4", n_clicks = 0, style = {'float':'right', 'background-color':'black'}),
+        html.Div(id = "popup-content2", children = [
+            dbc.Modal([
+                dbc.ModalHeader("Level 4", style = {'background-color':'grey'}),
+                dbc.ModalBody([
+                    dcc.Graph(id = 'Graph_2', figure = fig_lvl4),
+                    ],
+                    style = {'background-color':'white'}),
+            ], id = "popup2", fullscreen = True, centered = True)
+        ]),
+    ], style = {'width':'50%', 'height' :'50%', 'display':'inline-block'}),
+
     html.Div([
-        dcc.Graph(figure = fig_lvl5),
-    ], style = {'width':'48%', 'height' :'48%', 'display':'inline-block'}),
+        dcc.Graph(id = 'Graph_3', figure = fig_lvl5),
+        dbc.Button("Full Graph", id = "button_lvl5", n_clicks = 0, style = {'float':'right', 'background-color':'black'}),
+        html.Div(id = "popup-content3", children = [
+            dbc.Modal([
+                dbc.ModalHeader("Level 5", style = {'background-color':'grey'}),
+                dbc.ModalBody([
+                    dcc.Graph(id = 'Graph_3', figure = fig_lvl5),
+                    ],
+                    style = {'background-color':'white'}),
+            ], id = "popup3", fullscreen = True, centered = True)
+        ]),
+    ], style = {'width':'50%', 'height' :'50%', 'display':'inline-block'}),
+
     html.Div([
-        dcc.Graph(figure = fig_lvl6),
-    ], style = {'width':'48%', 'height' :'48%', 'display':'inline-block'})
+        dcc.Graph(id = 'Graph_4', figure = fig_lvl6),
+        dbc.Button("Full Graph", id = "button_lvl6", n_clicks = 0, style = {'float':'right', 'background-color':'black'}),
+        html.Div(id = "popup-content4", children = [
+            dbc.Modal([
+                dbc.ModalHeader("Level 6", style = {'background-color':'grey'}),
+                dbc.ModalBody([
+                    dcc.Graph(id = 'Graph_4', figure = fig_lvl6),
+                    ],
+                    style = {'background-color':'white'}),
+            ], id = "popup4", fullscreen = True, centered = True)
+        ]),
+    ], style = {'width':'50%', 'height' :'50%', 'display':'inline-block'})
 ])
 
-# Time series plot for 'Occupancy Overtime'
-traces = []
+# The overall skeleton 
+app.layout = html.Div([
+    dcc.Tabs(id = 'tabs', value = 'BarPlot',children = [
+        dcc.Tab(label = 'Overall Change in Occupancy', value = 'tab-1'),
+        dcc.Tab(label = 'Occupancy overtime', value = 'tab-2')
+    ]),
+    html.Div(id = 'tab-content')
+])
 
-for floor in floors:
-    trace = go.Scatter(
-        x=timestamps,
-        y=data[floor],
-        mode='lines',
-        name=floor,
-        line_shape='spline'
-    )
-    traces.append(trace)
-
-layout = go.Layout(
-    title='Central Library TimeSeries Plot by Floor',
-    xaxis=dict(
-        title='Time',
-        range=[time_range[0], time_range[1]],
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label='1H', step='hour', stepmode='backward'),
-                dict(count=6, label='6H', step='hour', stepmode='backward'),
-                dict(count=1, label='1D', step='day', stepmode='backward'),
-            ])
-        ),
-        type='date',
-        showline=False,
-        hoverformat=''
-    ),
-    yaxis=dict(
-        title='Occupancy Rate'
-    ),
-    legend=dict(
-        orientation='h'
-    )
+# Full graph button for the level 3 graph
+@app.callback(
+        Output('popup-content', 'style'),
+        Output('popup', 'is_open'),
+        Input('button_lvl3','n_clicks'),
+        State('popup', 'is_open')
 )
 
-layout['xaxis']['tickformat'] = '%H:%M'
+def toggle_popup(n1, is_open):
+    if n1:
+        return {"display":"block"},not is_open
+    return {"display":"none"}, is_open
 
-tab_oo_layout = dcc.Graph(figure={'data': traces, 'layout': layout})
+# Full graph button for the level 4 graph 
+@app.callback(
+        Output('popup-content2', 'style'),
+        Output('popup2', 'is_open'),
+        Input('button_lvl4','n_clicks'),
+        State('popup2', 'is_open')
+)
 
+def toggle_popup(n1, is_open):
+    if n1:
+        return {"display":"block"},not is_open
+    return {"display":"none"}, is_open
+
+# Full graph button for the level 5 graph
+@app.callback(
+        Output('popup-content3', 'style'),
+        Output('popup3', 'is_open'),
+        Input('button_lvl5','n_clicks'),
+        State('popup3', 'is_open')
+)
+
+def toggle_popup(n1, is_open):
+    if n1:
+        return {"display":"block"},not is_open
+    return {"display":"none"}, is_open
+
+# Full graph button for the level 6 graph 
+@app.callback(
+        Output('popup-content4', 'style'),
+        Output('popup4', 'is_open'),
+        Input('button_lvl6','n_clicks'),
+        State('popup4', 'is_open')
+)
+
+def toggle_popup(n1, is_open):
+    if n1:
+        return {"display":"block"},not is_open
+    return {"display":"none"}, is_open 
+
+# Output for the different tabs 
 @app.callback(
     Output('tab-content', 'children'),
     Input('tabs', 'value')
