0935bcf0689e8414a4c98b7985829dd9cceb206b
yanzhangdsa <141705113+yanzhangdsa@users.noreply.github.com>
2023-11-02 21:02:44 +0800

    Added timer and more spacing

--- frontend/loading_page/loading.py
+++ frontend/loading_page/loading.py
@@ -2,10 +2,13 @@ import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
-import dash_loading_spinners as dls  
+import dash_loading_spinners as dls
 
 app = dash.Dash(__name__)
 
+bahnschrift_style = {
+    'font-family': 'Bahnschrift, sans-serif'
+}
 
 app.layout = html.Div([
     html.Div(
@@ -13,24 +16,43 @@ app.layout = html.Div([
             dcc.Loading(
                 id="loading",
                 type="default",
-                fullscreen=True,  
-                color="navy",  # Text color
+                fullscreen=True,
+                color="navy",  
                 children=[
                     html.Div(
-                        "Loading",
+                        "Please wait...",
                         style={
                             "color": "navy",
                             "font-size": "24px",
-                            "display": "inline",
+                            "display": "block",
+                            "margin-bottom": "20px",  
+                            **bahnschrift_style,
                         },
                     ),
                     dcc.Loading(
                         type="default",
                         children=[
-                            dls.Ring(color="navy"),  
+                            dls.Ring(color="navy"),
+                            dcc.Interval(
+                                id='timer',
+                                interval=1000,  
+                                n_intervals=0
+                            ),
+                            html.Div(
+                                id='timer-output',
+                                children='Timer: 0s',
+                                style={**bahnschrift_style, "margin-top": "30px"}  
+                            )
                         ],
                     ),
                 ],
+                style={
+                    "position": "absolute",
+                    "top": "70%",  
+                    "left": "50%",
+                    "transform": "translate(-50%, -50%)",
+                    "text-align": "center",
+                },
             ),
         ],
         style={
@@ -42,6 +64,13 @@ app.layout = html.Div([
         },
     ),
 ])
+
+@app.callback(
+    Output('timer-output', 'children'),
+    Input('timer', 'n_intervals')
+)
+def update_timer(n):
+    return f'Timer: {n}s'
 
 if __name__ == "__main__":
     app.run_server(debug=True)
