0e0e60ee4135c2e146f65e16a77d912dc0aa739c
yanzhangdsa <141705113+yanzhangdsa@users.noreply.github.com>
2023-11-04 22:49:14 +0800

    Added home button, made the entire interface larger and buttons

--- frontend/run_simulation/runsimulation.py
+++ frontend/run_simulation/runsimulation.py
@@ -1,83 +1,71 @@
 import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
+from dash import dcc, html, Input, Output
+import base64
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.Link(
-        rel='stylesheet',
-        href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
-    ),
     html.Div([
-        html.Div([
-            html.H4("Choose Submission", style={'color': 'navy'}),
-            dcc.Dropdown(
-                options=[
-                    {'label': 'Option 1', 'value': 'option1'},
-                    {'label': 'Option 2', 'value': 'option2'},
-                    {'label': 'Option 3', 'value': 'option3'}
-                ],
-                style={'width': '150px', 'margin': 'auto'},
-            ),
-            html.I(className="fas fa-caret-down", style={'color': 'navy', 'margin-top': '20px'})
-        ], className="feature"),
-        
-        html.Div([
-            html.H4("Choose Period", style={'color': 'navy'}),
-            dcc.Dropdown(
-                options=[
-                    {'label': '9am ~ 10am', 'value': '9am ~ 10am'},
-                    {'label': '10am ~ 11am', 'value': '10am ~ 11am'},
-                    {'label': '11am ~ 12pm', 'value': '11am ~ 12pm'},
-                    {'label': '12pm ~ 1pm', 'value': '12pm ~ 1pm'},
-                    {'label': '1pm ~ 2pm', 'value': '1pm ~ 2pm'},
-                    {'label': '2pm ~ 3pm', 'value': '2pm ~ 3pm'},
-                    {'label': '3pm ~ 4pm', 'value': '3pm ~ 4pm'},
-                    {'label': '4pm ~ 5pm', 'value': '4pm ~ 5pm'},
-                    {'label': '5pm ~ 6pm', 'value': '5pm ~ 6pm'},
-                    {'label': '6pm ~ 7pm', 'value': '6pm ~ 7pm'},
-                    {'label': '7pm ~ 8pm', 'value': '7pm ~ 8pm'},
-                    {'label': '8pm ~ 8am', 'value': '8pm ~ 9pm'},
-                    {'label': '9am ~ 10am', 'value': '9pm ~ 10pm'},
-                    {'label': '10am ~ 11am', 'value': '10pm ~ 11pm'},
-                    {'label': '11am ~ 12pm', 'value': '11pm ~ 12am'},
-                    {'label': '12pm ~ 1pm', 'value': '12am ~ 1am'},
-                    {'label': '1pm ~ 2pm', 'value': '1am ~ 2am'},
-                    {'label': '2pm ~ 3pm', 'value': '2am ~ 3am'},
-                    {'label': '3pm ~ 4pm', 'value': '3am ~ 4am'},
-                    {'label': '4pm ~ 5pm', 'value': '4am ~ 5am'},
-                    {'label': '5pm ~ 6pm', 'value': '5am ~ 6am'},
-                    {'label': '6pm ~ 7pm', 'value': '6am ~ 7am'},
-                    {'label': '7pm ~ 8pm', 'value': '7am ~ 8am'}
-                ],
-                style={'width': '150px', 'margin': 'auto'},
-            ),
-        ], className="feature"),
-        
-        html.Div("OR", style={'color': 'navy', 'font-size': '20px', 'margin-top': '5px'}),  
-        
-        html.Div([
-            html.Button("Submit CSV", className="btn", id="submit-button", style={'margin-top': '10px'}),
-        ], className="feature"),
-        
-        html.Div([
-            html.I(className="fas fa-caret-down", style={'color': 'navy', 'margin-top': '20px'}),  
-        ], className="feature"),
-        
-        html.Div([
-            html.Button("Run Simulation", className="btn-run", style={'margin-top': '20px'})  
-        ], className="feature")
-    ], className="feature-container")
-], className="page", style={'text-align': 'center', 'padding-top': '50px'})
+        html.A(html.Button("Home", className="home-btn", id="home-button"), href="/"),
+    ], style={'position': 'absolute', 'top': '20px', 'left': '20px'}),  
 
-@app.callback(
-    Output('submit-button', 'n_clicks'),
-    Input('submit-button', 'n_clicks')
-)
-def trigger_upload(n_clicks):
-    return None
+    html.Div([
+        html.H3("Choose Submission", style={'color': 'navy', 'font-size': '24px'}),
+        dcc.Dropdown(
+            options=[
+                {'label': 'First Submission', 'value': 'option1'},
+                {'label': 'Second Submission', 'value': 'option2'},
+                {'label': 'Third Submission', 'value': 'option3'}
+            ],
+            style={'width': '150px', 'margin': 'auto'},
+        ),
+        html.I(className="fas fa-caret-down", style={'color': 'navy', 'font-size': '18px', 'margin-top': '10px'})
+    ], className="feature"),
+
+    html.Div([
+        html.H3("Choose Period", style={'color': 'navy', 'font-size': '24px'}),
+        dcc.Dropdown(
+            options=[
+                {'label': 'Exam Period', 'value': '9am ~ 10am'},
+                {'label': 'Event Period', 'value': '10am ~ 11am'},
+                {'label': 'Normal Period', 'value': '9am ~ 10am'},
+            ],
+            style={'width': '150px', 'margin': 'auto'},
+        ),
+    ], className="feature"),
+
+    html.Div("OR", style={'color': 'navy', 'font-size': '20px', 'margin-top': '10px'}),
+
+    html.Div([
+        dcc.Upload(
+            id='upload-data',
+            children=[
+                html.Button("Submit CSV", className="btn", id="submit-button", style={'font-size': '20px'}),
+            ],
+            style={'margin-top': '10px'},
+            multiple=False
+        ),
+    ], className="feature"),
+
+    html.Div([
+        html.I(className="fas fa-caret-down", style={'color': 'navy', 'font-size': '18px', 'margin-top': '10px'}),
+    ], className="feature"),
+
+    html.Div([
+        html.Button("Run Simulation", className="btn-run", style={'margin-top': '30px', 'font-size': '20px'})
+    ], className="feature"),
+    
+    html.Div(id='output-data-upload'),
+], className="feature-container", style={'text-align': 'center', 'padding-top': '50px'})
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'))
+def update_output(list_of_contents):
+    if list_of_contents is not None:
+        content = list_of_contents[0]
+        return content
 
 if __name__ == '__main__':
     app.run_server(debug=True)
