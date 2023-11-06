1fcde47fe8219042d5bbe33f57e75b01d2765eee
yanzhangdsa <141705113+yanzhangdsa@users.noreply.github.com>
2023-11-06 11:07:59 +0800

    Debugged

--- frontend/simulation_page/simulation_page.py
+++ frontend/simulation_page/simulation_page.py
@@ -1,5 +1,5 @@
 import dash
-import plotly.express as px 
+import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
 from dash import html, dcc, Input, Output, State
@@ -7,14 +7,13 @@ from datetime import datetime, timedelta
 import random
 import plotly.graph_objs as go
 
-# Initializing the app 
+# Initializing the app
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# data = pd.read_json("frontend/simulation_page/dummy_data/simulation_overall_change.json")
 data = pd.read_csv("frontend/simulation_page/dummy_data/trial_data_1lvl.csv")
-levels = list(data['level'].unique()) # identifying the levels that users have chosen to simulate
+levels = list(data['level'].unique())  # Identifying the levels that users have chosen to simulate
 
-# If user only input data for one level, the graph would occupy the whole screen else just half the screen
+# If the user only inputs data for one level, the graph would occupy the whole screen else just half the screen
 if len(levels) == 1:
     x = '100%'
     y = '100%'
@@ -22,95 +21,34 @@ else:
     x = "48%"
     y = "48%"
 
-for i in levels:
-    if i == 3:
-        # creating the dataframe for data of level 3
-        indexes_level3 = [index for index, value in enumerate(data['level']) if value == 3]
-        df_lvl3 = data.iloc[indexes_level3].sort_values(by = ' seat_type')
-        fig_lvl3 = px.bar(df_lvl3, x = ' seat_type', y = ' changeInOccupancy', title = 'Level 3', color = ' changeInOccupancy', 
-                                  color_discrete_sequence = ['red' if value <0 else 'green' for value in df_lvl3[' changeInOccupancy']],
-                                  text = df_lvl3[' changeInOccupancy'],
-                                  hover_data = {' changeInOccupancy': True})
+def create_level_layout(level, data):
+    indexes_level = [index for index, value in enumerate(data['level']) if value == level]
+    df_level = data.iloc[indexes_level].sort_values(by=' seat_type')
+    fig_level = px.bar(df_level, x=' seat_type', y=' changeInOccupancy', title=f'Level {level}', color=' changeInOccupancy',
+                       color_discrete_sequence=['red' if value < 0 else 'green' for value in df_level[' changeInOccupancy']],
+                       text=df_level[' changeInOccupancy'],
+                       hover_data={' changeInOccupancy': True})
 
-        fig_lvl3.update_traces(marker=dict(color=['red' if value <0 else 'green' for value in df_lvl3[' changeInOccupancy']])) # to ensure that the graph color would just be red and green 
-        fig_lvl3.update_layout(hovermode = 'closest')
+    fig_level.update_traces(marker=dict(color=['red' if value < 0 else 'green' for value in df_level[' changeInOccupancy']]))  # To ensure that the graph color would be red and green
+    fig_level.update_layout(hovermode='closest')
 
-        # the graph layout given the data from level 3
-        tab_bp_layout_level3 = html.Div([
-            dcc.Graph(id = 'Graph_1', figure = fig_lvl3),
-            dbc.Button("Full Graph", id = "button_lvl3", n_clicks = 0, style = {'float':'right', 'background-color':'lightlight blue'}),
-            html.Div(id = "popup-content", children = [
-                dbc.Modal([
-                    dbc.ModalHeader("Level 3", style = {'background-color':'lightblue', 'font-size': '24px', 'font-weight': 'bold'}),
-                    dbc.ModalBody([
-                        dcc.Graph(id = 'Graph_1', figure = fig_lvl3, style = {'width':'100%', 'height':'100%'}),
-                        ], style = {'background-color':'white'}),
-                        ], id = "popup", fullscreen = True, centered = True)
-                        ]),], style = {'width':x, 'height' :y, 'display': 'inline-block'})
+    # Create the layout for the given level
+    tab_bp_layout_level = html.Div([
+        dcc.Graph(id=f'Graph_{level}', figure=fig_level),
+        dbc.Button(f"Full Graph (Level {level})", id=f"button_lvl{level}", n_clicks=0, style={'float': 'right', 'background-color': 'lightblue'}),
+        html.Div(id=f"popup-content{level}", children=[
+            dbc.Modal([
+                dbc.ModalHeader(f"Level {level}", style={'background-color': 'lightblue', 'font-size': '24px', 'font-weight': 'bold'}),
+                dbc.ModalBody([
+                    dcc.Graph(id=f'Graph_{level}', figure=fig_level, style={'width': '100%', 'height': '100%'}),
+                ], style={'background-color': 'white'}),
+            ], id=f"popup{level}", fullscreen=True, centered=True)
+        ])
+    ], style={'width': x, 'height': y, 'display': 'inline-block'})
+    return tab_bp_layout_level
 
-    if i == 4:
-        indexes_level4 = [index for index, value in enumerate(data['level']) if value == 4]
-        df_lvl4 = data.iloc[indexes_level4].sort_values(by = ' seat_type')
-        fig_lvl4 = px.bar(df_lvl4, x = ' seat_type', y = ' changeInOccupancy', title = 'Level 4', color = ' changeInOccupancy', 
-                                  color_discrete_sequence = ['red' if value <0 else 'green' for value in df_lvl4[' changeInOccupancy']])
 
-        fig_lvl4.update_traces(marker=dict(color=['red' if value <0 else 'green' for value in df_lvl4[' changeInOccupancy']]))
-        tab_bp_layout_level4 = html.Div([
-            dcc.Graph(id = 'Graph_2', figure = fig_lvl4),
-            dbc.Button("Full Graph", id = "button_lvl4", n_clicks = 0, style = {'float':'right', 'background-color':'light blue'}),
-            html.Div(id = "popup-content2", children = [
-                dbc.Modal([
-                    dbc.ModalHeader("Level 4", style = {'background-color':'lightblue'}),
-                    dbc.ModalBody([
-                        dcc.Graph(id = 'Graph_2', figure = fig_lvl4, style = {'width': '100%', 'height': '100%'}),
-                        ],
-                        style = {'background-color':'white'}),
-                        ], id = "popup2", fullscreen = True, centered = True)
-                        ]),
-                        ], style = {'width':x, 'height' :y, 'display': 'inline-block'})
-    
-    if i == 5:
-        indexes_level5 = [index for index, value in enumerate(data['level']) if value == 5]
-        df_lvl5 = data.iloc[indexes_level5].sort_values(by = ' seat_type')
-        fig_lvl5 = px.bar(df_lvl5, x = ' seat_type', y = ' changeInOccupancy', title = 'Level 5', color = ' changeInOccupancy', 
-                                  color_discrete_sequence = ['red' if value <0 else 'green' for value in df_lvl5[' changeInOccupancy']])
-
-        fig_lvl5.update_traces(marker=dict(color=['red' if value <0 else 'green' for value in df_lvl5[' changeInOccupancy']]))
-        tab_bp_layout_level5 = html.Div([
-            dcc.Graph(id = 'Graph_3', figure = fig_lvl5),
-            dbc.Button("Full Graph", id = "button_lvl5", n_clicks = 0, style = {'float':'right', 'background-color':'light blue'}),
-            html.Div(id = "popup-content3", children = [
-                dbc.Modal([
-                    dbc.ModalHeader("Level 5", style = {'background-color':'lightblue'}),
-                    dbc.ModalBody([
-                        dcc.Graph(id = 'Graph_3', figure = fig_lvl5, style = {'width': '100%', 'height': '100%'}),
-                        ],
-                        style = {'background-color':'white'}),
-                        ], id = "popup3", fullscreen = True, centered = True)
-                        ]),
-                        ], style = {'width':x, 'height' :y, 'display': 'inline-block'})
-    
-    if i == 6:
-        indexes_level6 = [index for index, value in enumerate(data['level']) if value == 6]
-        df_lvl6 = data.iloc[indexes_level6].sort_values(by = ' seat_type')
-        fig_lvl6 = px.bar(df_lvl6, x = ' seat_type', y = ' changeInOccupancy', title = 'Level 6', color = ' changeInOccupancy', 
-                                  color_discrete_sequence = ['red' if value <0 else 'green' for value in df_lvl6[' changeInOccupancy']])
-
-        fig_lvl6.update_traces(marker=dict(color=['red' if value <0 else 'green' for value in df_lvl6[' changeInOccupancy']]))
-        tab_bp_layout_level6 = html.Div([
-            dcc.Graph(id = 'Graph_4', figure = fig_lvl6),
-            dbc.Button("Full Graph", id = "button_lvl6", n_clicks = 0, style = {'float':'right', 'background-color':'light blue'}),
-            html.Div(id = "popup-content4", children = [
-                dbc.Modal([
-                    dbc.ModalHeader("Level 6", style = {'background-color':'lightblue'}),
-                    dbc.ModalBody([
-                        dcc.Graph(id = 'Graph_4', figure = fig_lvl6, style = {'width': '100%', 'height': '100%'}),
-                        ],
-                        style = {'background-color':'white'}),
-                        ], id = "popup4", fullscreen = True, centered = True)
-                        ]),
-                        ], style = {'width':x, 'height' :y, 'display': 'inline-block'})
-
+level_layouts = {level: create_level_layout(level, data) for level in levels}
 
 # Sample data for four library floors
 floors = ["Floor 3", "Floor 4", "Floor 5", "Floor 6"]
@@ -122,73 +60,32 @@ data = {
     floor: [random.randint(0, 100) for _ in range(len(timestamps))] for floor in floors
 }
 
-# The overall skeleton 
+button_style = {'backgroundColor': 'black', 'color': 'white', 'borderRadius': '15px', 'margin': '5px'}
+
+# The overall skeleton
 app.layout = html.Div([
-    html.Button("Home", id = "button_home", n_clicks = 0, 
-                style = {'background-color': 'light grey', 'border-radius': '5px'}), 
-    html.Button(children = [html.Img(src = 'assets/close.png', style = {'width':'17px', 'height':'17px'})], 
-                id = "button_close", n_clicks = 0, 
-                style = {'border-radius':'5px', 'float':'right'}),
+    html.Button("Home", id="button_home", n_clicks=0, style={'background-color': 'light grey', 'border-radius': '5px'}),
+    html.Button(children=[html.Img(src='assets/close.png', style={'width': '17px', 'height': '17px'})], id="button_close", n_clicks=0, style={'border-radius': '5px', 'float': 'right'}),
     html.Div([
-        dcc.Tabs(id = 'tabs', value = 'BarPlot',children = [
-            dcc.Tab(label = 'Overall Change in Occupancy', value = 'tab-1', style = {'padding':'10px', 'background-color':'lightblue'}),
-            dcc.Tab(label = 'Occupancy overtime', value = 'tab-2', style = {'padding':'10px', 'background-color':'lightblue'})
-            ]),
-            html.Div(id = 'tab-content')
-            ])], style = {'padding':'10px'})
+        dcc.Tabs(id='tabs', value='tab-1', children=[
+            dcc.Tab(label='Overall Change in Occupancy', value='tab-1', style={'padding': '10px', 'background-color': 'lightblue'}),
+            dcc.Tab(label='Occupancy overtime', value='tab-2', style={'padding': '10px', 'background-color': 'lightblue'})
+        ]),
+        html.Div(id='tab-content')
+    ])], style={'padding': '10px'})
 
-# For 'full graph' button of level 3
-@app.callback(
-        Output('popup-content', 'style'),
-        Output('popup', 'is_open'),
-        Input('button_lvl3','n_clicks'),
-        State('popup', 'is_open')
-)
-
-def toggle_popup(n1, is_open):
-    if n1:
-        return {"display":"block"},not is_open
-    return {"display":"none"}, is_open
-
-# For 'full graph' button of level 4'
-@app.callback(
-        Output('popup-content2', 'style'),
-        Output('popup2', 'is_open'),
-        Input('button_lvl4','n_clicks'),
-        State('popup2', 'is_open')
-)
-
-def toggle_popup(n1, is_open):
-    if n1:
-        return {"display":"block"},not is_open
-    return {"display":"none"}, is_open
-
-# For full graph button of level 5
-@app.callback(
-        Output('popup-content3', 'style'),
-        Output('popup3', 'is_open'),
-        Input('button_lvl5','n_clicks'),
-        State('popup3', 'is_open')
-)
-
-def toggle_popup(n1, is_open):
-    if n1:
-        return {"display":"block"},not is_open
-    return {"display":"none"}, is_open
-
-# For full graph button of level 5
-@app.callback(
-        Output('popup-content4', 'style'),
-        Output('popup4', 'is_open'),
-        Input('button_lvl6','n_clicks'),
-        State('popup4', 'is_open')
-)
-
-def toggle_popup(n1, is_open):
-    if n1:
-        return {"display":"block"},not is_open
-    return {"display":"none"}, is_open
-    
+# Create callback functions for the "Full Graph" buttons for each level
+for level in levels:
+    @app.callback(
+        Output(f'popup-content{level}', 'style'),
+        Output(f'popup{level}', 'is_open'),
+        Input(f'button_lvl{level}', 'n_clicks'),
+        State(f'popup{level}', 'is_open')
+    )
+    def toggle_popup(n1, is_open, level=level):
+        if n1:
+            return {"display": "block"}, not is_open
+        return {"display": "none"}, is_open
 
 # Time series plot for 'Occupancy Overtime'
 traces = []
@@ -200,7 +97,7 @@ for floor in floors:
         mode='lines',
         name=floor,
         line_shape='spline',
-        opacity=1  # Set initial opacity to 1 for all traces
+        opacity=1  
     )
     traces.append(trace)
 
@@ -229,8 +126,6 @@ layout = go.Layout(
 
 layout['xaxis']['tickformat'] = '%H:%M'
 
-button_style = {'backgroundColor': 'black', 'color': 'white', 'borderRadius': '15px', 'margin': '5px'}
-
 tab_oo_layout = html.Div([
     dcc.Graph(figure={'data': traces, 'layout': layout}, id='tab-oo-layout'),
     html.Div([
@@ -241,61 +136,16 @@ tab_oo_layout = html.Div([
     ], style={'text-align': 'center'}),
 ])
 
+# Callback for switching between tabs and displaying the full graphs
 @app.callback(
     Output('tab-content', 'children'),
-    Output('tab-oo-layout', 'figure'),
-    Input('tabs', 'value'),
-    Input('level-3-button', 'n_clicks'),
-    Input('level-4-button', 'n_clicks'),
-    Input('level-5-button', 'n_clicks'),
-    Input('level-6-button', 'n_clicks')
+    Input('tabs', 'value')
 )
-def update_content_and_trace(tab, level3_clicks, level4_clicks, level5_clicks, level6_clicks):
-    to_show = []
-    if tab == 'tab-1':  
-        for i in levels:
-            if i == 3:
-                to_show.append(tab_bp_layout_level3)
-            if i == 4:
-                to_show.append(tab_bp_layout_level4)
-            if i == 5:
-                to_show.append(tab_bp_layout_level5)
-            if i == 6:
-                to_show.append(tab_bp_layout_level6)
-        tab_content = html.Div(to_show)
-    else:  
-        tab_content = tab_oo_layout
+def update_content(tab):
+    if tab == 'tab-1':
+        return [level_layouts[level] for level in levels]
+    else:
+        return tab_oo_layout
 
-    ctx = dash.callback_context
-    button_id = ctx.triggered[0]['prop_id'].split('.')[0]  
-
-    for trace in traces:
-        trace['opacity'] = 1
-
-    if button_id == 'level-3-button':
-        traces[0]['opacity'] = 1
-        traces[1]['opacity'] = 0.2
-        traces[2]['opacity'] = 0.2
-        traces[3]['opacity'] = 0.2
-    elif button_id == 'level-4-button':
-        traces[0]['opacity'] = 0.2
-        traces[1]['opacity'] = 1
-        traces[2]['opacity'] = 0.2
-        traces[3]['opacity'] = 0.2
-    elif button_id == 'level-5-button':
-        traces[0]['opacity'] = 0.2
-        traces[1]['opacity'] = 0.2
-        traces[2]['opacity'] = 1
-        traces[3]['opacity'] = 0.2
-    elif button_id == 'level-6-button':
-        traces[0]['opacity'] = 0.2
-        traces[1]['opacity'] = 0.2
-        traces[2]['opacity'] = 0.2
-        traces[3]['opacity'] = 1
-
-    trace_figure = {'data': traces, 'layout': layout}
-    
-    return tab_content, trace_figure
-    
 if __name__ == '__main__':
-    app.run_server(debug = True)
+    app.run_server(debug=True)
