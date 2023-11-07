49de68d3fb7084a7002c8e3ac4a33b8241482f72
claudeonrs <claudeon9j6@gmail.com>
2023-11-07 10:52:33 +0800

    Update attributes in class to generate section graphs

--- lib_sections.py
+++ lib_sections.py
@@ -13,7 +13,8 @@ def dict_to_graph(library_dict: dict, sections_attrs: dict, connections: dict):
         G.add_node(level_id)
         for sections_dict in levels_dict['sections']:
             section_id = f"{levels_dict['level']}-{sections_dict['seat_type']}"
-            G.add_node(section_id, **sections_dict, **sections_attrs[sections_dict['seat_type']], level=levels_dict['level'], empty_seats=sections_dict['capacity'])
+            G.add_node(section_id, **sections_dict, **sections_attrs[sections_dict['seat_type']], level=levels_dict['level'],
+                       empty_seats=sections_dict['count']*sections_attrs[sections_dict['seat_type']]['num_seats'], capacity=sections_dict['count']*sections_attrs[sections_dict['seat_type']]['num_seats'])
             G.add_edge(level_id, section_id)
     for connection in connections['gates']:
         G.add_edge(connection['gate_id'], connection['connected_to'])
